895ecf177cdf834a3cde6511b640304c8c70f7be
Alexandre Pereira <116118540+AlexandrePereira12@users.noreply.github.com>
2025-01-29 22:09:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -121,15 +121,6 @@ document.getElementById('login-form').addEventListener('submit', function(event)
 
 
 
-document.getElementById('cpf').addEventListener('input', function(e) {
-    var value = e.target.value;
-    var cpfPattern = value.replace(/\D/g, '') // Remove qualquer coisa que não seja número
-                          .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o terceiro dígito
-                          .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o sexto dígito
-                          .replace(/(\d{3})(\d)/, '$1-$2') // Adiciona traço após o nono dígito
-                          .replace(/(-\d{2})\d+?$/, '$1'); // Impede entrada de mais de 11 dígitos
-    e.target.value = cpfPattern;
-  });
 
 // Armazena os pedidos no localStorage
 document.getElementById('checkout-form').addEventListener('submit', function(event) {
